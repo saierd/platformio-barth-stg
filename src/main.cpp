60f50fd07aa668a4ccb776ad9306f551e772f592
Daniel Saier <mail@danielsaier.de>
2022-01-22 21:15:19 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#undef LED_BUILTIN
+#define LED_BUILTIN 8
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+}
