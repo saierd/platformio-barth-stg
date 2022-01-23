e356427277ea2dc51719e59a1e59239e1f7e81a5
Daniel Saier <mail@danielsaier.de>
2022-01-23 18:05:40 +0100

    Add a header with more user friendly names for the Arduino pin numbers

---
+++ src/pins.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <pins_arduino.h>
+
+// Pin definitions according to the description on the board.
+// These are the same pin numbers as in the original Arduino core for the STG-820 board.
+// See https://barth-elektronik.com/downloads/9022-0022-A.pdf
+#define IN1 A0
+#define IN2 A1
+#define IN3 A3
+#define IN4 6
+#define IN5 7
+#define OUT1 0
+#define OUT2 1
+#define OUT3 2
+#define OUT4 3
+#define OUT5 4  // Analog.
